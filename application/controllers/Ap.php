2da0040800fddfd9d604038b5498135a8a3d6927
siossan <love_kojopi@hotmail.co.jp>
2016-09-30 18:56:27 +0900

    m

--- application/controllers/Ap.php
+++ application/controllers/Ap.php
@@ -129,8 +129,41 @@ class Ap extends MY_Controller {
             $result = $this->Aps_model->getAps();
         }
         header("Content-Type: application/json; charset=utf-8");
-        echo json_encode( $result , 256 );
+
+
+
+        echo $this->json_xencode($result);
         exit;
+    }
+
+    /**
+     * PHP5.4からでないと対応していないUnicodeアンエスケープをPHP5.3でもできるようにしたラッパー関数
+     * @param mixed   $value
+     * @param int     $options
+     * @param boolean $unescapee_unicode
+     */
+    function json_xencode($value, $options = 0, $unescapee_unicode = true) {
+        $v = json_encode($value, $options);
+        if ($unescapee_unicode) {
+            $v = $this->unicode_encode($v);
+            // スラッシュのエスケープをアンエスケープする
+            $v = preg_replace('/\\\\\//', '/', $v);
+        }
+        return $v;
+    }
+
+    /**
+     * Unicodeエスケープされた文字列をUTF-8文字列に戻す。
+     * 参考:http://d.hatena.ne.jp/iizukaw/20090422
+     * @param unknown_type $str
+     */
+    function unicode_encode($str) {
+        
+        return preg_replace_callback("/\\\\u([0-9a-zA-Z]{4})/", array(get_class($this), 'encode_callback'), $str);
+    }
+
+    function encode_callback($matches) {
+        return mb_convert_encoding(pack("H*", $matches[1]), "UTF-8", "UTF-16");
     }
 
 }
